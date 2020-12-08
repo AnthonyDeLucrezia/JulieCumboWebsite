8a483a05ddb8105d662577dd72f670761788e1dc
Anthony Delucrezia <Anthony.Delucrezia@devoteam.lu>
2020-12-08 16:34:26 +0100

    quote home page and question row

--- src/modules/Home/index.jsx
+++ src/modules/Home/index.jsx
@@ -1,17 +1,51 @@
 import React from "react";
 import "./index.scss";
-import Carousel from "./../../components/carousel/index.jsx";
+import AppButton from "./../../components/button/index.jsx";
 import { Row, Col } from "react-bootstrap";
-import AppTitle from "./../../components/title/index.jsx";
 
 const Home = () => {
   return (
     <>
+      <div className="home-slider">
+        <img src={`images/pexels-evg-culture-1170979.jpg`} alt="First slide" />
+        <div className="home-slider-content">
+          <div className="home-slider-text">
+            <h1>Appoitments</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+              justo metus, mattis non lobortis at, hendrerit nec sem. Donec
+              aliquam lacus nec urna maximus posuere. Curabitur ornare nisi eget
+              fermentum
+            </p>
+          </div>
+        </div>
+      </div>
       <Row>
-        <Col>{/* <Carousel /> */}</Col>
+        <Col className="home-about-us">
+          <h1>Notre cabinet</h1>
+          <blockquote class="home-quote">
+            <img class="img-circle" src="images/julie.jpg" alt="" />
+            <p>
+              <q>
+                I urgently required dental help last 4th of July. And despite
+                all other clinics being closed, BeDentist took my appointment!
+              </q>
+            </p>
+            <div>
+              <cite>Julie Cumbo</cite>
+              <time datetime="2018-04-30">– May 10, 2018</time>
+            </div>
+          </blockquote>
+        </Col>
       </Row>
-      <Row>
-        <Col>{/* <AppTitle>bonjour</AppTitle> */}</Col>
+      <Row className="help-row">
+        <Col md={{ span: 3, offset: 2 }}>
+          <h1>Des questions ?</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Col>
+        <Col md={{ span: 3, offset: 1 }} className="help-button">
+          <AppButton text={"Nous contacter"} />
+        </Col>
       </Row>
     </>
   );
