d04aa2c88d7dce616b95eacf7a9a9c2924d5b26d
Anthony Delucrezia <Anthony.Delucrezia@devoteam.lu>
2020-12-10 23:24:16 +0100

    fix vontact

--- src/modules/Contact/index.jsx
+++ src/modules/Contact/index.jsx
@@ -1,54 +1,54 @@
 import "./index.scss";
 import React from "react";
-import { Form,Button,Container,Col,Row } from "react-bootstrap";
+import { Form, Button, Container, Col, Row } from "react-bootstrap";
 import MapContainer from "/src/components/googleMaps/index.jsx";
 
-const FormContact = () =>
-(
+const FormContact = () => (
   <div className="w3-padding-48">
-      <h4 className="w3-wide w3-margin-bottom-32 w3-center">CONTACTEZ-NOUS</h4>
-      <Form>
-        <Form.Row>
-          <Form.Group as={Col} controlId="formGridName">
-            <Form.Label>Nom patient</Form.Label>
-            <Form.Control type="text"/>
-          </Form.Group>
-          <Form.Group as={Col} controlId="formGridSurname">
-            <Form.Label>Prénom patient</Form.Label>
-            <Form.Control type="text"/>
-          </Form.Group>
-          <Form.Group as={Col} controlId="formGridBirthdate">
-            <Form.Label>Date de naissance patient</Form.Label>
-            <Form.Control type="date"/>
-          </Form.Group>
-        </Form.Row>
-
-        <Form.Row>
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email"/>
-          </Form.Group>
-          <Form.Group as={Col} controlId="formGridPhone">
-            <Form.Label>Téléphone</Form.Label>
-            <Form.Control type="tel"/>
-          </Form.Group>
-        </Form.Row>
-
-        <Form.Group controlId="forGridMessage">
-          <Form.Label>Message</Form.Label>
-          <Form.Control as="textarea" rows={4} />
+    <h4 className="w3-wide w3-margin-bottom-32 w3-center">CONTACTEZ-NOUS</h4>
+    <Form>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridName">
+          <Form.Label>Nom patient</Form.Label>
+          <Form.Control type="text" />
         </Form.Group>
+        <Form.Group as={Col} controlId="formGridSurname">
+          <Form.Label>Prénom patient</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group as={Col} controlId="formGridBirthdate">
+          <Form.Label>Date de naissance patient</Form.Label>
+          <Form.Control type="date" />
+        </Form.Group>
+      </Form.Row>
 
-        <Button variant="primary" type="submit" block>Submit</Button>
-      </Form>
-    </div>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" />
+        </Form.Group>
+        <Form.Group as={Col} controlId="formGridPhone">
+          <Form.Label>Téléphone</Form.Label>
+          <Form.Control type="tel" />
+        </Form.Group>
+      </Form.Row>
+
+      <Form.Group controlId="forGridMessage">
+        <Form.Label>Message</Form.Label>
+        <Form.Control as="textarea" rows={4} />
+      </Form.Group>
+
+      <Button variant="primary" type="submit" block>
+        Submit
+      </Button>
+    </Form>
+  </div>
 );
 
-const Infos = () =>
-(
+const Infos = () => (
   <div className="w3-padding-48">
-      <h3 className="w3-wide w3-margin-bottom-32 w3-center">INFOS PRATIQUES</h3>
-      <Container>
+    <h3 className="w3-wide w3-margin-bottom-32 w3-center">INFOS PRATIQUES</h3>
+    <Container>
       <Row className="w3-row-padding">
         <Col>
           <p>E-mail :</p>
@@ -67,24 +67,18 @@ const Infos = () =>
   </div>
 );
 
-const Map = () =>
-(
+const Map = () => (
   <div className="w3-center w3-content w3-container googleMap">
     <MapContainer />
   </div>
 );
 
-const Contact = () => {
-  return 
-  (
-    <> 
-      <div className="w3-padding-48 w3-content">
-        <FormContact />
-        <Infos />
-        <Map />
-      </div>
-    </>
-  );
-};
+const Contact = () => (
+  <div className="w3-padding-48 w3-content">
+    <FormContact />
+    <Infos />
+    <Map />
+  </div>
+);
 
 export { Contact as default };
