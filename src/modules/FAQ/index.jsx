9d6a1f9df8daae094254272fe5cf93eeda86d9fd
Anthony Delucrezia <Anthony.Delucrezia@devoteam.lu>
2020-12-21 21:34:31 +0100

    instagram feed - blog

--- src/modules/FAQ/index.jsx
+++ src/modules/FAQ/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import "./index.scss";
 
 const FAQ = () => {
-  return <div className={"faq-container"}></div>;
+  return (
+    <div className={"faq-container"}>
+      <Row className="faq-title">
+        <Col>
+          <h1>F.A.Q</h1>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export { FAQ as default };
