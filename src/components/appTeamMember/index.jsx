779edda8a167c1108d867f92f33765310f11282e
Anthony Delucrezia <Anthony.Delucrezia@devoteam.lu>
2021-03-22 21:36:18 +0100

    team page v2

--- src/components/appTeamMember/index.jsx
+++ src/components/appTeamMember/index.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-regular-svg-icons";
 import "./index.scss";
 
 const AppTeamMember = (props) => {
+  const { t } = useTranslation();
   const { className, img, title, subTitle, description } = props;
+  const [displayDesc, setDisplayDesc] = useState(false);
 
   let badgeClassName = "app-team-container";
 
   if (className) badgeClassName = `${badgeClassName} ${className}`;
 
+  const onClick = () => {
+    setDisplayDesc(!displayDesc);
+  };
+
   return (
     <div className={badgeClassName}>
-      <div className="team-img-container">
-        <img src={img} alt="team-member" />
+      <div className="team-header">
+        <div className="team-img-container">
+          <img src={img} alt="team-member" />
+        </div>
+        <div className="team-title-container">
+          {title && <div className="team-title">{title}</div>}
+          {subTitle && <div className="team-subtitle">{subTitle}</div>}
+        </div>
+        <div className="team-btn-container">
+          <span onClick={onClick}>
+            <FontAwesomeIcon icon={faEye} size="lg" />
+            {displayDesc ? t("team.buttonMoins") : t("team.buttonPlus")}
+          </span>
+        </div>
       </div>
-      <div className="team-body">
-        {title && <div className="team-title">{title}</div>}
-        {subTitle && <div className="team-subtitle">{subTitle}</div>}
-        {description && <p className="team-desc">{description}</p>}
-      </div>
+      {displayDesc && (
+        <div className="team-body">
+          {description && <p className="team-desc">{description}</p>}
+        </div>
+      )}
     </div>
   );
 };
